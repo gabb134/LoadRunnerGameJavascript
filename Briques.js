ebdb4b045c48e5d7ed0f5559a3e8275cc760bc07
gabriel <cg.marrero@cfodin.qc.ca>
2020-02-24 13:46:08 -0500

    changement pour des classes

---
+++ Briques.js
@@ -0,0 +1,10 @@
+class Briques {
+    constructor(positionX,positionY,positionFinX,positionFinY){
+        this.positionX = positionX;
+        this.positionY = positionY;
+
+        this.positionFinX = positionFinX;
+        this.positionFinY = positionFinY;
+    }
+    
+}
